975e408ada81409047ff4445b1b92d07144e259d
SMPC-006\ECO0537_KHANHND <khanhnguyen1@tdt.asia>
2021-11-10 11:47:57 +0700

    map

--- components/MapView/index.tsx
+++ components/MapView/index.tsx
@@ -1,15 +1,17 @@
+import { StandaloneSearchBox } from '@react-google-maps/api'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
-import { withGoogleMap, withScriptjs, GoogleMap, Marker, OverlayView, InfoWindow } from 'react-google-maps'
+import { withGoogleMap, withScriptjs, GoogleMap, Marker, OverlayView, InfoWindow, DirectionsRenderer } from 'react-google-maps'
 import InfoBox from 'react-google-maps/lib/components/addons/InfoBox'
-import SearchBox from 'react-google-maps/lib/components/places/SearchBox'
-import StandaloneSearchBox from 'react-google-maps/lib/components/places/StandaloneSearchBox'
+// import 
 
 const MapView = () => {
   const [coordinates, setCoordinates] = useState({ lat: 21.017555, lng: 105.783073 })
   const [infoBoxHover, setInfoBoxHover] = useState(false)
   const [infoBoxClick, setInfoBoxClick] = useState(false)
+  const [directions, setDirections] = useState<any>()
   const [markers, setMarkers] = useState()
+  const [place, setPlace] = useState([])
   const marker = { lat: 21.032739, lng: 105.792463 }
 
   const [order, setOrder] = useState({
@@ -20,80 +22,106 @@ const MapView = () => {
     status: 0
   })
 
+  let refs: any
+
+  useEffect(() => {
+    const DirectionsService = new google.maps.DirectionsService();
+
+    DirectionsService.route({
+      origin: new google.maps.LatLng(coordinates),
+      destination: new google.maps.LatLng(marker),
+      travelMode: google.maps.TravelMode.DRIVING,
+    }, (result, status) => {
+      if (status === google.maps.DirectionsStatus.OK) {
+        setDirections(result)
+      } else {
+        console.error(`error fetching directions ${result}`);
+      }
+    })
+  }, [])
+
   return (
-    <GoogleMap
-      defaultZoom={15}
-      defaultCenter={coordinates}
-    >
-
-      <StandaloneSearchBox
-        // controlPosition={google.maps.ControlPosition.TOP_LEFT}
-        onPlacesChanged={() => { }}
-        bounds={{ east: 0, north: 0, south: 0, west: 0 }}
+    <>
+      <GoogleMap
+        defaultZoom={15}
+        defaultCenter={coordinates}
+        // defaultOptions={{ mapId: 'a9b5f754b0b222ed' }}
       >
-        <input
-          type="text"
-          placeholder="Customized your placeholder"
-          style={{
-            boxSizing: `border-box`,
-            border: `1px solid transparent`,
-            width: `240px`,
-            height: `32px`,
-            marginTop: `27px`,
-            padding: `0 12px`,
-            borderRadius: `3px`,
-            boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-            fontSize: `14px`,
-            outline: `none`,
-            textOverflow: `ellipses`,
+        <Marker
+          position={marker}
+          onClick={() => setInfoBoxClick(true)}
+          onMouseOver={() => setInfoBoxHover(true)}
+          onMouseOut={() => setInfoBoxHover(false)}
+        >
+          <InfoBox options={{ closeBoxURL: '', enableEventPropagation: true }}  >
+            <div style={{ backgroundColor: 'yellow', padding: 10 }}>
+              <span style={{ fontWeight: 'bold', fontSize: 13 }}>Đơn online mới !</span>
+            </div>
+          </InfoBox>
+          {(infoBoxHover || infoBoxClick) && <InfoWindow
+            onCloseClick={() => setInfoBoxClick(false)}
+          // onLoad={onLoad}
+          >
+            <div style={{}}>
+              <h4>{order.order_title}</h4>
+              <h5>{`Khách hàng: ${order.customer_name}`}</h5>
+              <p style={{ margin: 0, padding: 0 }}>{`Khoảng cách: ${order.distance}`}</p>
+              <p style={{ margin: 0, padding: 0 }}>{order.amount ? ('Tổng tiền: ' + order.amount) : 'Tổng tiền: Đang cập nhật'}</p>
+              {order.status === 0 && <button onClick={() => { }} className="button">Nhận đơn</button>}
+            </div>
+          </InfoWindow>}
+        </Marker>
+
+        <Marker
+          icon={{
+            url: 'https://insulationpads.co.uk/wp-content/uploads/2017/10/Home.png',
+            scaledSize: new google.maps.Size(40, 40),
           }}
-        />
-      </StandaloneSearchBox>
-
-      <Marker
-        position={marker}
-        onClick={() => setInfoBoxClick(true)}
-        onMouseOver={() => setInfoBoxHover(true)}
-        onMouseOut={() => setInfoBoxHover(false)}
-      >
-        <InfoBox options={{ closeBoxURL: '', enableEventPropagation: true }}  >
-          <div style={{ backgroundColor: 'yellow', padding: 10 }}>
-            <span style={{ fontWeight: 'bold', fontSize: 13 }}>Đơn online mới !</span>
-          </div>
-        </InfoBox>
-        {(infoBoxHover || infoBoxClick) && <InfoWindow
-          onCloseClick={() => setInfoBoxClick(false)}
-        // onLoad={onLoad}
+          position={coordinates}
+          title="Medlink"
+          defaultTitle="Medlink"
         >
-          <div style={{}}>
-            <h4>{order.order_title}</h4>
-            <h5>{`Khách hàng: ${order.customer_name}`}</h5>
-            <p style={{ margin: 0, padding: 0 }}>{`Khoảng cách: ${order.distance}`}</p>
-            <p style={{ margin: 0, padding: 0 }}>{order.amount ? ('Tổng tiền: ' + order.amount) : 'Tổng tiền: Đang cập nhật'}</p>
-            {order.status === 0 && <button onClick={() => { }} className="button">Nhận đơn</button>}
-          </div>
-        </InfoWindow>}
-      </Marker>
+          <InfoBox
+            options={{ closeBoxURL: '', enableEventPropagation: true }}
+          >
+            <div style={{ backgroundColor: 'transparent', color: 'green', borderRadius: '1em' }}>
+              <span style={{ fontWeight: 'bold', fontSize: 14 }}>Nhà thuốc Medlink</span>
+            </div>
+          </InfoBox>
+        </Marker>
 
-      <Marker
-        icon={{
-          url: 'https://insulationpads.co.uk/wp-content/uploads/2017/10/Home.png',
-          scaledSize: new google.maps.Size(40, 40),
-        }}
-        position={coordinates}
-        title="Medlink"
-        defaultTitle="Medlink"
-      >
-        <InfoBox
-          options={{ closeBoxURL: '', enableEventPropagation: true }}
+        {directions && <DirectionsRenderer directions={directions} />}
+
+        <StandaloneSearchBox
+          ref={(ref: any) => {
+            console.log(ref)
+            refs = ref
+          }}
+          onPlacesChanged={() => {
+            console.log(refs)
+            setPlace(refs.getPlaces())
+          }}
         >
-          <div style={{ backgroundColor: 'transparent', color: 'green', borderRadius: '1em' }}>
-            <span style={{ fontWeight: 'bold', fontSize: 14 }}>Nhà thuốc Medlink</span>
-          </div>
-        </InfoBox>
-      </Marker>
-
-    </GoogleMap>
+          <input
+            type="text"
+            placeholder="Customized your placeholder"
+            style={{
+              boxSizing: `border-box`,
+              border: `1px solid transparent`,
+              width: `240px`,
+              height: `32px`,
+              marginTop: `27px`,
+              padding: `0 12px`,
+              borderRadius: `3px`,
+              boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+              fontSize: `14px`,
+              outline: `none`,
+              textOverflow: `ellipses`,
+            }}
+          />
+        </StandaloneSearchBox>
+      </GoogleMap>
+    </>
   )
 }
 
--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+import StandaloneSearchBox from 'react-google-maps/lib/components/places/StandaloneSearchBox';
 import MapView from '../../components/MapView';
 
 const Map = () => {
   const key = 'AIzaSyC2u32QZBnGewrIdVtFXhFwf87aD2yiEB8'
+  const [place, setPlace] = useState([])
   return (
     <>
       <h1> This is map </h1>
@@ -12,11 +14,12 @@ const Map = () => {
         <Link href="/">Home</Link>
       </h2>
       <MapView
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${key}`}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${key}&libraries=places`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `90vh`, margin: `auto`, border: '2px solid black' }} />}
         mapElement={<div style={{ height: `100%` }} />}
       />
+
     </>
   )
 }
